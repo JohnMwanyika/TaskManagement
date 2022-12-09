17a7d1b8bd54b1642220681e3632a88ed5227f00
johnMwanyika <mwanyikajohn@outlook.com>
2022-12-09 10:19:05 +0300

    Aggregates

--- controllers/dashboard.controller.js
+++ controllers/dashboard.controller.js
@@ -5,6 +5,37 @@ module.exports = {
   mainDashboard: async (req, res) => {
     try {
       if (req.session.user) {
+        // count Actove tasks
+        const groupActive = await prisma.task.aggregate({
+          _count: {
+            status_id: true
+          },
+          where: {
+            status_id: 1,
+            user_id:req.session.user.user_id
+          },
+        });
+        // Count Completed tasks
+        const grpCompleted = await prisma.task.aggregate({
+          _count: {
+            status_id: true,
+          },
+          where: {
+            status_id: 2,
+            user_id:req.session.user.user_id
+          },
+        });
+        // count all tasks
+        const grpAll = await prisma.task.aggregate({
+          _count: {
+            task_id: true,
+          },
+          where: {
+            user_id: req.session.user.user_id,
+          },
+        });
+        console.log(grpAll);
+        // get all tasks
         const allTasks = await prisma.task.findMany({
           include: { user: true, task_status: true },
           orderBy: {
@@ -19,17 +50,23 @@ module.exports = {
           bd: "Dashboard",
           card_title: "Recent Tasks",
           user: req.session.user,
-          pass:req.session.pass,
+          pass: req.session.pass,
           // shows the sign in toast
-          toast:'fire',
+          toast: "fire",
           // requires moment in the rendered template
-          moment: require('moment')
+          moment: require("moment"),
+          // total Active Tasks
+          totalActive: groupActive,
+          // total Completed Tasks
+          totalCompleted: grpCompleted,
+          // total tasks of logged in user
+          totalTasks:grpAll
         });
       } else {
         res.render("login", { message: "You need to login first" });
       }
     } catch (error) {
-      res.render("not found", { message: error.message });
+      res.render("not_found", { message: error.message });
     }
   },
 };
