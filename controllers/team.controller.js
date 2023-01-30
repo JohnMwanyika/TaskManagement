2c9d8858b6d42f46a015ad419c3a6f0258c3b669
John Mwanyika <87723962+JohnMwanyika@users.noreply.github.com>
2023-01-30 17:22:38 +0300

    fix send sms

--- controllers/team.controller.js
+++ controllers/team.controller.js
@@ -34,24 +34,24 @@ module.exports = {
         .then(
           axiosSendSMS(
             user.phone,
-            `Hello ${user.first_name}, ${req.session.user.first_name} has added you to a project login to check`
+            `Hello ${user.first_name}, ${req.session.user.first_name} has added you to a project login to check your tasks`
           )
         )
-        .then(res.redirect("back"));
+        .then(res.redirect('../../projects'));
     } else {
       res.json({ message: { info: "This user exists", type: "error" } });
     }
   },
   removeFromTeamById: async (req, res) => {
     // const { userId, projectId } = req.params;
-    const { userId, projectId } = req.body;
-    console.log('user is',userId)
-    console.log('project is',projectId)
+    const { user, project } = req.body;
+    console.log('user is',user)
+    console.log('project is',project)
     const team = await prisma.team.deleteMany({
       // include: { project: true, userId: true },
       where: {
-        projectProject_id: parseInt(projectId),
-        userUser_id: parseInt(userId),
+        projectProject_id: parseInt(project),
+        userUser_id: parseInt(user),
       },
     }).then(res.redirect('back'))
     // .then(res.json({message:"confirm user removed from project"}));
--- middlewares/sendSms.js
+++ middlewares/sendSms.js
@@ -1,31 +1,66 @@
 const axios = require("axios");
+
+module.exports = async function axiosSendText(phone, message) {
+  var data = JSON.stringify({
+    response_type: "json",
+    sender_name: "23107",
+    service_id: 0,
+    message: message,
+    mobile: phone,
+  });
+
+  let config = {
+    method: "post",
+    url: "https://api.mobitechtechnologies.com/sms/sendsms",
+    data: data,
+    headers: {
+      h_api_key:
+        "373a889e83bef0fda31e496b7ed5e4378e2de25fb6d2207c00a457774ff4876a",
+      "Content-Type": "application/json",
+    },
+  };
+
+  await axios(config).then((res) => {
+    console.log(JSON.stringify(res.data));
+  }).catch((err)=>{
+    console.log(err)
+  })
+};
 // const request = require("ajax-request");
 
-const url = "https://api.mobitechtechnologies.com/sms/sendsms";
-const api_key =
-  "373a889e83bef0fda31e496b7ed5e4378e2de25fb6d2207c00a457774ff4876a";
-const sender_id = "23107";
+// const api_key =
+//   "373a889e83bef0fda31e496b7ed5e4378e2de25fb6d2207c00a457774ff4876a";
+// const sender_id = "23107";
 
-module.exports = async function axiosSendSMS(phone, message) {
-  let details = {
-    api_key: api_key,
-    username:'23107',
-    mobile_number: '0'+phone,
-    message: message,
-    sender_id: 23107,
-  };
-  try {
-    const response = await axios.post(url, details);
-    console.log("request successful!");
-    console.log(response);
-  } catch (error) {
-    if (error.response) {
-      console.log(error.reponse.status);
-    } else {
-      console.log(error.message);
-    }
-  }
-};
+// module.exports = async function axiosSendSMS(phone, message) {
+//   let url = "https://api.mobitechtechnologies.com/sms/sendsms";
+
+//   let details = {
+//     mobile_number: "0" + phone,
+//     response_type: "json",
+//     message: message,
+//     sender_name: 23107,
+//     service_id: 0,
+//   };
+
+//   let headers = {
+//     h_api_key:
+//       "373a889e83bef0fda31e496b7ed5e4378e2de25fb6d2207c00a457774ff4876a",
+//     "Content-Type": "application/json",
+//   };
+
+//   try {
+//     const response = await axios.post(url, details, headers);
+//     console.log("request successful!");
+//     console.log(response);
+//   } catch (error) {
+//     if (error.response) {
+//       console.log(error.reponse.status);
+//     } else {
+//       console.log(error.message);
+//     }
+//   }
+// };
 
 // const AfricasTalking = require("africastalking");
 
@@ -49,24 +84,32 @@ module.exports = async function axiosSendSMS(phone, message) {
 //     console.error(ex);
 //   }
 // };
-
+// #########AJAX SEND ###############
 // module.exports = function ajaxSendText(phone, message) {
 //   try {
-//     const api_key =
-//       "373a889e83bef0fda31e496b7ed5e4378e2de25fb6d2207c00a457774ff4876a";
 //     const url = "http://bulksms.mobitechtechnologies.com/api/sendsms";
+
+//     var details = {
+//       mobile_number: "0" + phone,
+//       response_type: "json",
+//       message: message,
+//       sender_name: "23107",
+//       service_id: 0,
+//     };
+//     var data = JSON.stringify(details)
+
+//     var headers = {
+//       h_api_key:
+//         "373a889e83bef0fda31e496b7ed5e4378e2de25fb6d2207c00a457774ff4876a",
+//       "Content-Type": "application/json;charset=UTF-8",
+//     //   "Access-Control-Allow-Origin": "*",
+//     };
 
 //     request.post(
 //       {
 //         url: url,
-//         data: {
-//           api_key: api_key,
-//           username: "23107",
-//           sender_id: "23107",
-//           message: message,
-//           phone: "0" + phone,
-//         },
-//         // headers: {},
+//         data: data,
+//         headers: headers,
 //       },
 //       function (err, res, body) {
 //         if (err) {
