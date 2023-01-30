9e2737412648d7b915eba53ea4f49d12f13a0da8
John Mwanyika <87723962+JohnMwanyika@users.noreply.github.com>
2023-01-30 23:54:26 +0300

    sms to inform user of project unassignment

--- controllers/team.controller.js
+++ controllers/team.controller.js
@@ -14,7 +14,13 @@ module.exports = {
         user_id: parseInt(userId),
       },
     });
-
+    // get project Name
+    const projectTitle = await prisma.project.findUnique({
+      select: { title: true },
+      where: {
+        project_id: parseInt(projectId),
+      },
+    });
     const existingUser = await prisma.team.findMany({
       include: { project: true, userId: true },
       where: {
@@ -34,10 +40,10 @@ module.exports = {
         .then(
           axiosSendSMS(
             user.phone,
-            `Hello ${user.first_name}, ${req.session.user.first_name} has added you to a project login to check your tasks`
+            `Hello ${user.first_name}, ${req.session.user.first_name} has added you to ${projectTitle.title} project, login to check your tasks`
           )
         )
-        .then(res.redirect('../../projects'));
+        .then(res.redirect("../../projects"));
     } else {
       res.json({ message: { info: "This user exists", type: "error" } });
     }
@@ -45,16 +51,40 @@ module.exports = {
   removeFromTeamById: async (req, res) => {
     // const { userId, projectId } = req.params;
     const { user, project } = req.body;
-    console.log('user is',user)
-    console.log('project is',project)
-    const team = await prisma.team.deleteMany({
-      // include: { project: true, userId: true },
+    // get the name of removed user
+    const removedUser = await prisma.user.findUnique({
+      select: { first_name: true, phone: true },
       where: {
-        projectProject_id: parseInt(project),
-        userUser_id: parseInt(user),
+        user_id: parseInt(user),
       },
-    }).then(res.redirect('back'))
+    });
+    // get project Name
+    const projectTitle = await prisma.project.findUnique({
+      select: { title: true },
+      where: {
+        project_id: parseInt(project),
+      },
+    });
+
+    console.log("user is", user);
+    console.log("project is", project);
+    // remove user from selected project
+    const team = await prisma.team
+      .deleteMany({
+        // include: { project: true, userId: true },
+        where: {
+          projectProject_id: parseInt(project),
+          userUser_id: parseInt(user),
+        },
+      })
+      .then(
+        axiosSendSMS(
+          removedUser.phone,
+          `Hello ${removedUser.first_name} ${req.session.user.first_name} removed you from the ${projectTitle.title} project`
+        )
+      )
+      .then(res.redirect("back"));
     // .then(res.json({message:"confirm user removed from project"}));
-    console.log(team)
+    console.log(team);
   },
 };
